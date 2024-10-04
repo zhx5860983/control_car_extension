5a08340452b573d256d89cf0142cad1c8dba2895
zhx5860983 <134288730+zhx5860983@users.noreply.github.com>
2024-10-04 20:24:24 +0200

    follow naming conventions

--- custom.ts
+++ custom.ts
@@ -14,179 +14,178 @@ enum MotorSetupMode {
 /**
  * Custom blocks
  */
-//% weight=100 color=#0fbc11
-namespace ControlCar {
+//% weight=100 color=#0fbc11 icon="\uf2db" block="Control Car"
+namespace controlCar {
 
-    let straight_pressed = 0
+    let straightPressed = 0
 
-    let velo_r_target = 0.0
-    let velo_l_target = 0.0
-    let velo_multiplier_r = 1.0
-    let velo_multiplier_l = 1.0
-    let full_speed = 0.0
-    let speed_limit = 100.0
+    let veloTargetR = 0.0
+    let veloTargetL = 0.0
+    let veloMultiplierR = 1.0
+    let veloMultiplierL = 1.0
+    let fullSpeed = 0.0
+    let speedLimit = 100.0
 
-    let pure_rotation_rate = 0.3
-    let mixed_rotation_rate = 0.2
+    let pureRotationRate = 0.3
+    let mixedRotationRate = 0.2
 
-    let motor_setup_mode = MotorSetupMode.Direct;
+    let motorSetupMode = MotorSetupMode.Direct;
     let dt = 0.0
-    let time_current = 0.0
-    let time_last = 0.0
-    let velo_r = 0.0
-    let velo_l = 0.0
-    let acceleration_time = 50
-    let inv_acceleration_time = 1.0 / acceleration_time
+    let timeCurrent = 0.0
+    let timeLast = 0.0
+    let veloR = 0.0
+    let veloL = 0.0
+    let accelerationTime = 50
+    let invAccelerationTime = 1.0 / accelerationTime
 
     /**
-     * @param speed_limit_ is the highest rotational speed of motors
-     * @param pure_rotation_rate_ is the speed of motors (as a rate of highest speed) used for pure rotation
-     * @param mixed_rotation_rate_ is the speed of motors (as a rate of highest speed) used for mixed rotation
-     * @param acceleration_time_ is the rate of current RPM convergening to terget RPM
-     * @param motor_setup_ is Reversed, when motor1 and motor2 corresponse to wheel_right and wheel_left, otherwise 0
+     * @param speedLimit_ is the highest rotational speed of motors
+     * @param pureRotationRate_ is the speed of motors (as a rate of highest speed) used for pure rotation
+     * @param mixedRotationRate_ is the speed of motors (as a rate of highest speed) used for mixed rotation
+     * @param accelerationTime_ is the rate of current RPM convergening to terget RPM
+     * @param motorSetup_ is Reversed, when motor1 and motor2 corresponse to wheel_right and wheel_left, otherwise 0
      */
-    //% block="Car servo control || with maxRPM $speed_limit_|PRR $pure_rotation_rate_|MRR $mixed_rotation_rate_|acceleration time $acceleration_time_|and $motor_setup_|motor setup|"
-    //% speed_limit_.min=20.0 pure_rotation_rate_.max=100.0 
-    //% speed_limit_.defl=100.0
-    //% pure_rotation_rate_.min=0.0 pure_rotation_rate_.max=1.0
-    //% pure_rotation_rate_.defl=0.3
-    //% mixed_rotation_rate_.min=0.0 mixed_rotation_rate_.max=0.6
-    //% mixed_rotation_rate_.defl=0.2
-    //% acceleration_time_.min=20 acceleration_time_.max=1000
-    //% acceleration_time_.defl=0
-    //% acceleration_time_.shadow=timePicker
+    //% block="car servo control || with maxRPM $speedLimit_|PRR $pureRotationRate_|MRR $mixedRotationRate_|acceleration time $accelerationTime_|and $motorSetup_|motor setup"
+    //% speedLimit_.min=20.0 speedLimit_.max=100.0
+    //% speedLimit_.defl=100.0
+    //% pureRotationRate_.min=0.0 pureRotationRate_.max=1.0
+    //% pureRotationRate_.defl=0.3
+    //% mixedRotationRate_.min=0.0 mixedRotationRate_.max=0.6
+    //% mixedRotationRate_.defl=0.2
+    //% accelerationTime_.min=20 accelerationTime_.max=1000
+    //% accelerationTime_.defl=0
+    //% accelerationTime_.shadow=timePicker
     //% expendableArgumentMode="toggle"
-    //% motor_setup_.defl=MotorSetup.Reversed
+    //% motorSetup_.defl=MotorSetup.Reversed
     //% weight=50
     //% inlineInputMode=inline
-    export function car_servo_speed(speed_limit_: number, pure_rotation_rate_: number, mixed_rotation_rate_: number,
-        acceleration_time_: number, motor_setup_: MotorSetup): void {
+    export function carServoSpeed(speedLimit_: number, pureRotationRate_: number, mixedRotationRate_: number,
+        accelerationTime_: number, motorSetup_: MotorSetup): void {
 
-        speed_limit = speed_limit_
-        pure_rotation_rate = pure_rotation_rate_
-        mixed_rotation_rate = mixed_rotation_rate_
+        speedLimit = speedLimit_
+        pureRotationRate = pureRotationRate_
+        mixedRotationRate = mixedRotationRate_
 
-        if (Math.abs(acceleration_time_) < 20) {
-            motor_setup_mode = MotorSetupMode.Direct
+        if (Math.abs(accelerationTime_) < 20) {
+            motorSetupMode = MotorSetupMode.Direct
         } else {
-            motor_setup_mode = MotorSetupMode.Sevro
-            acceleration_time = acceleration_time_
-            inv_acceleration_time = 1.0 / acceleration_time
+            motorSetupMode = MotorSetupMode.Sevro
+            accelerationTime = accelerationTime_
+            invAccelerationTime = 1.0 / accelerationTime
         }
 
         bluetooth.startButtonService()
         bluetooth.startLEDService()
 
         // ---------- control panel setup --------------
-        function set_target_velo() {
-            velo_l_target = full_speed * velo_multiplier_l
-            velo_r_target = full_speed * velo_multiplier_r
+        function setTargetVelo() {
+            veloTargetL = fullSpeed * veloMultiplierL
+            veloTargetR = fullSpeed * veloMultiplierR
         }
 
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_DOWN, function () {
-            straight_pressed = 1
-            full_speed = speed_limit
-            set_target_velo()
+            straightPressed = 1
+            fullSpeed = speedLimit
+            setTargetVelo()
         })
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_1_UP, function () {
-            straight_pressed = 0
-            full_speed = 0.0
-            set_target_velo()
+            straightPressed = 0
+            fullSpeed = 0.0
+            setTargetVelo()
         })
 
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_DOWN, function () {
-            straight_pressed = 1
-            full_speed = - speed_limit
-            set_target_velo()
+            straightPressed = 1
+            fullSpeed = - speedLimit
+            setTargetVelo()
         })
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_2_UP, function () {
-            straight_pressed = 0
-            full_speed = 0.0
-            set_target_velo()
+            straightPressed = 0
+            fullSpeed = 0.0
+            setTargetVelo()
         })
 
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_3_DOWN, function () {
-            if (straight_pressed) {
-                velo_multiplier_l = mixed_rotation_rate
+            if (straightPressed) {
+                veloMultiplierL = mixedRotationRate
             } else {
-                full_speed = speed_limit
-                velo_multiplier_l = -pure_rotation_rate
-                velo_multiplier_r = pure_rotation_rate
+                fullSpeed = speedLimit
+                veloMultiplierL = -pureRotationRate
+                veloMultiplierR = pureRotationRate
             }
-            set_target_velo()
+            setTargetVelo()
         })
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_3_UP, function () {
-            if (!(straight_pressed)) {
-                full_speed = 0.0
+            if (!(straightPressed)) {
+                fullSpeed = 0.0
             }
-            velo_multiplier_l = 1.0
-            velo_multiplier_r = 1.0
-            set_target_velo()
+            veloMultiplierL = 1.0
+            veloMultiplierR = 1.0
+            setTargetVelo()
         })
 
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_4_DOWN, function () {
-            if (straight_pressed) {
-                velo_multiplier_r = mixed_rotation_rate
+            if (straightPressed) {
+                veloMultiplierR = mixedRotationRate
             } else {
-                full_speed = speed_limit
-                velo_multiplier_r = -pure_rotation_rate
-                velo_multiplier_l = pure_rotation_rate
+                fullSpeed = speedLimit
+                veloMultiplierR = -pureRotationRate
+                veloMultiplierL = pureRotationRate
             }
-            set_target_velo()
+            setTargetVelo()
         })
         control.onEvent(EventBusSource.MES_DPAD_CONTROLLER_ID, EventBusValue.MES_DPAD_BUTTON_4_UP, function () {
-            if (!(straight_pressed)) {
-                full_speed = 0.0
+            if (!(straightPressed)) {
+                fullSpeed = 0.0
             }
-            velo_multiplier_l = 1.0
-            velo_multiplier_r = 1.0
-            set_target_velo()
+            veloMultiplierL = 1.0
+            veloMultiplierR = 1.0
+            setTargetVelo()
         })
         // ---------- control panel setup --------------
 
         basic.forever(function () {
-            if (motor_setup_mode == MotorSetupMode.Sevro) {
-                if (Math.abs(time_last) < 1e-5) {
-                    time_current = input.runningTime()
+            if (motorSetupMode == MotorSetupMode.Sevro) {
+                if (Math.abs(timeLast) < 1e-5) {
+                    timeCurrent = input.runningTime()
                 }
-                time_current = input.runningTime()
-                dt = time_current - time_last
-                time_last = time_current
+                timeCurrent = input.runningTime()
+                dt = timeCurrent - timeLast
+                timeLast = timeCurrent
 
-                velo_l = velo_l + inv_acceleration_time * (velo_l_target - velo_l) * dt
-                velo_r = velo_r + inv_acceleration_time * (velo_r_target - velo_r) * dt
-                if (motor_setup_ == MotorSetup.Reversed)
-                    wuKong.setAllMotor(velo_r, velo_l)
-                else
-                    wuKong.setAllMotor(velo_l, velo_r)
+                veloL = veloL + invAccelerationTime * (veloTargetL - veloL) * dt
+                veloR = veloR + invAccelerationTime * (veloTargetR - veloR) * dt
             } else {
-                if (motor_setup_ == MotorSetup.Reversed)
-                    wuKong.setAllMotor(velo_r_target, velo_l_target)
-                else
-                    wuKong.setAllMotor(velo_l_target, velo_r_target)
+                veloL = veloTargetL
+                veloR = veloTargetR
             }
+
+            if (motorSetup_ == MotorSetup.Reversed)
+                wuKong.setAllMotor(veloR, veloL)
+            else
+                wuKong.setAllMotor(veloL, veloR)
         })
     }
 
     //% block="Reset car speed"
     //% weight=10
-    export function reset_car_speed() {
-        straight_pressed = 0
-        velo_l_target = 0.0
-        velo_r_target = 0.0
-        velo_multiplier_l = 1.0
-        velo_multiplier_r = 1.0
+    export function resetCarSpeed() {
+        straightPressed = 0
+        veloTargetL = 0.0
+        veloTargetR = 0.0
+        veloMultiplierL = 1.0
+        veloMultiplierR = 1.0
 
-        velo_l = 0.0
-        velo_r = 0.0
-        time_current = 0.0
+        veloL = 0.0
+        veloR = 0.0
+        timeCurrent = 0.0
         dt = 0.0
-        time_last = 0.0
+        timeLast = 0.0
     }
 
     //% block="Reset car"
     //% weight=5
-    export function reset_car() {
+    export function resetCar() {
         control.reset()
     }
 
