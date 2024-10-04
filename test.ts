eb0a26af269e741bb93685283bf32c6dd16bbf92
zhx5860983 <134288730+zhx5860983@users.noreply.github.com>
2024-10-04 21:22:20 +0200

    Update main.blocks, test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,1 @@
-
-input.onButtonPressed(Button.A, function () {
-    controlCar.resetCarSpeed()
-})
-input.onButtonPressed(Button.B, function () {
-    controlCar.resetCar()
-})
 controlCar.carServoSpeed(100, 0.3, 0.2, 0, MotorSetup.Reversed)
